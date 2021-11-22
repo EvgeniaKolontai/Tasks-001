553d7156d008f5953f27007bfb3cc489ff6fe3c9
Evgenia <kolontai.e.v@yandex.ru>
2021-11-22 11:00:12 +0300

    Change 2

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int result1= numberA * numberA;
+Console.WriteLine(result1);
+
+int numberB = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(numberB);
+int result2= numberB * numberB;
+Console.WriteLine(result2);
+
+if (result1 == result2)
+{
+    Console.WriteLine("Квадрат числа");
+}
+
+else
+{
+  Console.WriteLine("НеВерно");  
+}
+
+
