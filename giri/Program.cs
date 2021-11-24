493ee95ffabfcb975953374aed752c16f15c812b
Evgenia <kolontai.e.v@yandex.ru>
2021-11-24 14:10:32 +0300

    change 9

---
+++ giri/Program.cs
@@ -0,0 +1,15 @@
+﻿int []array = { 1, 2, 4, 6, 12, 16, 17, 20, 22, 24};
+
+int n = array.Length;
+int find = 22;
+
+int index = 0;
+
+while(index < n)
+{
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+    }
+     index++;  
+}
