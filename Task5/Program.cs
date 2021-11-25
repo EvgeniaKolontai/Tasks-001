8a9e7f3b5236875245dc4c1094ee52b7fe68dd47
Evgenia <kolontai.e.v@yandex.ru>
2021-11-25 11:48:36 +0300

    Change

---
+++ Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу вычисления значения функции y = f(a)
+Console.Clear(); 
+ 
+int number = new Random().Next(1, 20); 
+double f(double a) 
+{ 
+    return (a*a+1); 
+} 
+Console.WriteLine(number); 
+Console.WriteLine(f(number));
