f2af0163a28906c46c85813f6371499d78eb50cc
Evgenia <kolontai.e.v@yandex.ru>
2021-11-25 10:58:52 +0300

    Change

---
+++ Task3/Program.cs
@@ -0,0 +1,4 @@
+﻿string[] Weeks = {"  ", "mon", "thu", "wen", "thr", "fra", "sat", "sun"};
+Console.WriteLine("Enter number:  ");
+int index = int.Parse(Console.ReadLine());
+Console.WriteLine(Weeks[index]);
