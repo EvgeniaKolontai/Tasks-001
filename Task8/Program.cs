2f02087e94cb3d1f90637834a5de947ffb9a3e06
Evgenia <kolontai.e.v@yandex.ru>
2021-11-25 20:09:07 +0300

    Change 1

---
+++ Task8/Program.cs
@@ -0,0 +1,37 @@
+﻿//Показать четные числа от 1 до N
+Console.Clear(); 
+void FillArray(int[] collect);
+int length = collect.Length;
+int index = 0;
+
+{
+    int [] num = {1, 2, 3, 4};
+}
+{
+    int length = num.Length;
+    int index = 0;
+    while (index < length)
+        {
+            if (num[index] %2 == 0)
+            {
+                FillArray(collect);  
+            }
+
+            else 
+                {
+                    (num[index] %2 != 0);
+                }
+            
+        index++;
+        }
+    return position;
+}
+FillArray(collect);
+PrintArray(collect);
+
+
+
+
+
+
+
