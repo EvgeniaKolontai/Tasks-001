71f969be0c1b8263695ff2956bc4ade46417685b
Evgenia <kolontai.e.v@yandex.ru>
2021-11-26 21:05:11 +0300

    Change

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,30 +1,18 @@
 ﻿//Показать четные числа от 1 до N
+
 Console.Clear(); 
-void FillArray(int[] collect);
-int length = collect.Length;
-int index = 0;
+Console.Write("our num is ");
+int N = new Random().Next(1, 200);
+Console.WriteLine(N);
+int FirstNum = 1;
+int NextNum = FirstNum + 1;
 
+while (NextNum < N)
 {
-    int [] num = {1, 2, 3, 4};
+    if (NextNum %2 == 0)
+    {
+        Console.WriteLine(NextNum);
+    
+    }
+   NextNum = NextNum + 2;  
 }
-{
-    int length = num.Length;
-    int index = 0;
-    while (index < length)
-        {
-            if (num[index] %2 == 0)
-            {
-                FillArray(collect);  
-            }
-
-            else 
-                {
-                    (num[index] %2 != 0);
-                }
-            
-        index++;
-        }
-    return position;
-}
-FillArray(collect);
-PrintArray(collect);
