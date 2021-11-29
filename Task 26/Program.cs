c5c08613866eea7534a58c672aa94699bd9a157e
Evgenia <kolontai.e.v@yandex.ru>
2021-11-29 14:43:23 +0300

    Change 1

---
+++ Task 26/Program.cs
@@ -0,0 +1,18 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.Clear();
+Console.WriteLine("Enter num ");
+int A = int.Parse(Console.ReadLine());
+int B = new Random ().Next(2, 5);
+int result = 1;
+
+
+for (int i = 0; i < B; i++)
+{
+    result =  result * A ;
+    
+}
+    Console.WriteLine(B);
+    Console.WriteLine(result);
+  
+
--- Task 9/Program.cs
+++ Task 9/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Clear(); 
+﻿//Показать последнюю цифру трёхзначного числа
+
+Console.Clear(); 
 int N = new Random ().Next(100,999);
 Console.WriteLine(N);
-Console.WriteLine(N %100);
+Console.WriteLine(N %10);
