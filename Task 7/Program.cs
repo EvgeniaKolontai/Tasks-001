9b970f3869cbab1cc4feae8f02eeccf3f8874ef6
Evgenia <kolontai.e.v@yandex.ru>
2021-11-29 11:16:12 +0300

    Change 1

---
+++ Task 7/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать числа от -N до N
+
+Console.WriteLine("Enter num");
+int FirstNum = int.Parse(Console.ReadLine());
+int NextNum = -FirstNum;
+    while (NextNum <= FirstNum)
+    {
+       Console.WriteLine(NextNum);
+       NextNum++; 
+    }
+
+
+
+    
+
+
