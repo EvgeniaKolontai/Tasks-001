fb904b70c4329c092bad6f1858c4a3f9df7aeb21
Evgenia <kolontai.e.v@yandex.ru>
2021-11-29 15:23:32 +0300

    Change1

---
+++ Task 25/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму чисел от 1 до А
+Console.Clear();
+Console.WriteLine("Enter num ");
+int A = int.Parse(Console.ReadLine());
+int sum = 0;
+
+for (int i = 1; i <= A ; i++)
+{
+  sum = i + sum;
+}
+
+Console.WriteLine(sum);
