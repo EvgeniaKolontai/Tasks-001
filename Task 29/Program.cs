831afb07d389727a2ff72968434232173200d267
Evgenia <kolontai.e.v@yandex.ru>
2021-11-30 15:00:01 +0300

    Change

---
+++ Task 29/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+Console.WriteLine("Enter num ");
+int A = int.Parse(Console.ReadLine());
+int P = 1;
+for (int i = 1; i <= A ; i++)
+{
+  P = i * P;
+  Console.Write($"{i}*");
+}
+
+Console.WriteLine($"={P}");
