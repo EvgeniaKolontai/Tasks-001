09381e481b4d571e29589515c485b370371fe76d
Evgenia <kolontai.e.v@yandex.ru>
2021-11-30 13:47:54 +0300

    Change1

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,3 +1,14 @@
-﻿Console.Clear(); 
-int number = new Random().Next(1, 200);
+﻿//Дано число. Проверить кратно ли оно 7 и 23
+Console.Clear(); 
+Console.WriteLine("Enter number ");
+int number = int.Parse(Console.ReadLine());
+
+    if (number %7 == 0 || number%23 ==0)
+{
+    Console.WriteLine("Делится на 7,23");
+}
+else
+    {
+        Console.WriteLine("Не кратно 7 и 23");
+    }
 
