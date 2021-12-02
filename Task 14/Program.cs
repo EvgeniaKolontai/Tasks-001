7e6ff0a179ab566b52e7b9bb5c1c8074903c20e2
Evgenia <kolontai.e.v@yandex.ru>
2021-12-02 10:58:23 +0300

    Change

---
+++ Task 14/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.Clear();
+
+void Find3Num( string NumberA)
+{
+if (NumberA.Length >2)
+{
+    Console.WriteLine($"{NumberA[2]}");
+}
+else
+{
+    Console.WriteLine("Нет,nретьей цифры");
+}
+}
+Console.Write("Enter number: ");
+//string NumberA = Console.ReadLine();
+Find3Num(Console.ReadLine());
