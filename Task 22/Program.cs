bcfb49c69f679009df3cd390dd47624a2d3376fe
Evgenia <kolontai.e.v@yandex.ru>
2021-12-02 12:02:40 +0300

    Chdnge

---
+++ Task 22/Program.cs
@@ -0,0 +1,37 @@
+﻿// Найти расстояние между точками в пространстве 2D/3D
+
+Console.Clear();
+double Dist(double x, double y, double z)
+{
+    double XYZ = Math.Round(Math.Sqrt(Math.Pow(x, 2)+Math.Pow(y, 2)+Math.Pow(z, 2)), 2);
+    return XYZ;
+}
+
+Console.Write("Какое пространство 2D или 3D? (2/3): ");
+int count = Convert.ToInt32(Console.ReadLine());
+
+if (count !=2 && count !=3) 
+{
+    Console.Write("Недопустимое значение");
+    return;
+}
+
+double x = 0;
+double y = 0;
+double z = 0;
+for (int i = 0; i < 2; i++)
+{
+    Console.Write($"Введите координату Х точку №{i+1}: ");
+    x = Convert.ToInt32(Console.ReadLine()) -x;
+    Console.Write($"Введите координату y точку №{i+1}: ");
+    y = Convert.ToInt32(Console.ReadLine()) -y;
+
+    if (count  == 3)
+    {
+        Console.Write($"Введите координату z точку №{i+1}: ");
+    z = Convert.ToInt32(Console.ReadLine()) -z;
+    }
+    Console.WriteLine($"Координаты точки№{i+1}: ({x}, {y}, {z})");
+}
+
+Console.WriteLine($"Расстояние между точками равно: {Dist(x, y, z)}");
