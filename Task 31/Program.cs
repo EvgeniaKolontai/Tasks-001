316d8ed92bb6a9da2a5d5431aad4f4452d53f02f
Evgenia <kolontai.e.v@yandex.ru>
2021-12-02 17:37:17 +0300

    Change

---
+++ Task 31/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+
+int[] arr = new int[] { 11, 211, 31, 41, 15, 61, 17, 18 };
+int n = arr.Length;
+var str = string.Join(" ", arr);
+ Console.WriteLine(str);
+// Console.Write("{0}", string.Join(", ", arr));
+
+
+
+
+        
+
+
