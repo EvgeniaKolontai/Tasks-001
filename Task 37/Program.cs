3c10f10d5a5580e27077154096492fbaa0f8d002
Evgenia <kolontai.e.v@yandex.ru>
2021-12-09 12:52:09 +0300

    Change1

---
+++ Task 37/Program.cs
@@ -0,0 +1,25 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+Console.Clear();
+
+int[] array = new int[123];
+Random rnd = new Random();
+for (int i = 0; i < array.Length; i++)
+      {
+          array[i] = rnd.Next(0, 20);
+          Console.Write($" {array[i]} ");
+      } 
+      //for (int i = 0; i < array.Length; i++)
+      //{
+        //  array[i] = i+1;
+        //  Console.Write($" {array[i]} ");
+     // }
+
+int count =0; 
+for (int index = 0; index < array.Length; index++)
+{
+    if (array[index] >= 10 && array[index] <= 99)
+    {
+        count = count + 1;
+    }
+}
+Console.WriteLine($"\n Количество элементов внутри отрезка от 10 до 99: {count}");
