94680821252b9a0108cf4ae715cd62da7028eb5e
Evgenia <kolontai.e.v@yandex.ru>
2021-12-13 13:46:39 +0300

    Change 7

---
+++ Task 20/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.Clear();
+Console.WriteLine("Введите номер чертверти: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x < 1 || x >= 5) Console.WriteLine("Не верное значение четверти");
+if (x == 1) Console.WriteLine(" x = от 0 до бесконечности, y = от 0 до бесконечности");
+if (x == 2) Console.WriteLine(" x = от -0 до бесконечности, y = от 0до бесконечности");
+if (x == 3) Console.WriteLine(" x = от -0 до бесконечности, y = от -0 до бесконечности");
+if (x == 4) Console.WriteLine(" x = от 0 до бесконечности, y = от -0 до бесконечности");
+
+
+
+    
+
+
