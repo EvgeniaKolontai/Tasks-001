09ab14b7071cb2a7e1a85533d671bb8d05f62e3f
Evgenia <kolontai.e.v@yandex.ru>
2021-12-13 12:01:28 +0300

    Change5

---
+++ Task 44/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти точку пересечения двух прямых заданных
+// уравнением y = k1 * x + b1, y = k2 * x + b2 (b1 k1 и b2 и k2 заданы)
+// x=(b2-b1)/(k1-k2)
+int b1 = new Random().Next(-10, 11);
+int b2 = new Random().Next(-10, 11);
+int k1 = new Random().Next(-10, 11);
+int k2 = new Random().Next(-10, 11);
+
+if (k1 == k2)
+{
+    Console.WriteLine("Линии совпадают или параллельны");
+    return;
+}
+
+Console.WriteLine($"Координата х :{(b2-b1)/(k1-k2)}");
+Console.WriteLine($"Координата y :{k1 *(b2-b1)/(k1-k2) + b1}");
