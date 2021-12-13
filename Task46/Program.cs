3562633c9654803c871e262962078e24976baba4
Evgenia <kolontai.e.v@yandex.ru>
2021-12-13 11:35:53 +0300

    Chang4

---
+++ Task46/Program.cs
@@ -0,0 +1,31 @@
+﻿// 46. Написать программу масштабирования фигуры
+// Console.Write($"({X[IndexX]*k},");
+string s = (Console.ReadLine());
+// if ()
+// Console.Write($"({X[IndexX]*k},");
+double k = 2;
+char[] separators = new char[] { ' ', ',', '(', ')' };
+string[] Nums = (s.Split(separators, StringSplitOptions.RemoveEmptyEntries));
+
+int[] X = new int[(Nums.Length)/2];
+int[] Y = new int[(Nums.Length)/2];
+  int Index = 0;
+for (int i = 0; i < Nums.Length; i++)
+{
+    if (i%2==0)
+    {
+        X[Index] = (Convert.ToInt32(Nums[i]));
+    
+        Console.Write($"({X[Index]*k},");
+    
+    }
+    else 
+    {
+        Y[Index] = (Convert.ToInt32(Nums[i]));
+        
+        Console.Write($" {Y[Index]*k}) ");
+        Index++;
+        
+    }
+
+}
