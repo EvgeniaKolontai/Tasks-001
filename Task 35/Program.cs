858a89b1c3fc8fe197be3b0783d9d1e160604eaf
Evgenia <kolontai.e.v@yandex.ru>
2021-12-13 14:28:30 +0300

    Change

---
+++ Task 35/Program.cs
@@ -0,0 +1,30 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число
+Console.Clear();
+Console.Write("Введите размер массивa length = ");
+int length = int.Parse(Console.ReadLine());
+int[] array = new int [length];
+ for (int i = 0; i < length; i++)
+ {
+     array[i] = new Random().Next(1, 10);
+     Console.Write($" {array[i]} ");
+
+ }
+ Console.WriteLine();
+ Console.WriteLine("Введите число: ");
+ int a = int.Parse(Console.ReadLine());
+ bool b = true;
+ for (int i = 0; i < length; i++)
+ {
+     if(a == array[i]) 
+    {
+        b = false;
+        Console.WriteLine("Число присутствует");
+        break;  
+
+    }
+ }
+
+if (b)
+{
+    Console.WriteLine("Число не присутствет");
+}
