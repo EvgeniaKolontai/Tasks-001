83c2244d8822a1ab0c16e44828566cf1b7e9d246
Evgenia <kolontai.e.v@yandex.ru>
2021-12-16 11:03:47 +0300

    Change

---
+++ Task 38/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+Console.Clear();
+int [] arr = {1,2,3,4,5,6,7,8,9};
+int sum = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (i%2==1)
+    {
+         sum = sum + arr[i];
+    }
+
+}
+Console.WriteLine($"Сумма элементов {sum}");
+
+
