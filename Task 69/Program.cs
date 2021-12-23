a0566f4150b1238304ae74562e1ac39509e05460
Evgenia <kolontai.e.v@yandex.ru>
2021-12-23 11:54:19 +0300

    Change1

---
+++ Task 69/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+void SumNum( int N, int M, int sum)
+{
+    if (M == N) 
+    {
+    sum = sum+M; Console.WriteLine(M); 
+    Console.WriteLine($"Сумма чисел равна {sum}");return;
+     }
+    if (M < N) { sum = sum+M;Console.WriteLine(M); SumNum(N, M + 1 , sum); }
+    else if (M > N) {sum = sum+M; Console.WriteLine(M); SumNum(N, M - 1, sum); }
+
+}
+SumNum(5, 8, 0);
